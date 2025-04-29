ce9880ea22892b5a054f804570cde0214ee5076f
prem <prempandey0722@gmail.com>
2025-04-29 11:20:35 +0530

    add StatsSection component with background image and statistics display

---
+++ src/app/Dinning/page.jsx
@@ -0,0 +1,17 @@
+
+import StatsSection from "../../components/Dinning/StatsSection.jsx";
+
+import ClientsSay from "../../components/HomePageComp/ClientsSay.jsx"
+
+const page = () => {
+  return (
+    <div>
+         
+        <StatsSection/>
+        <ClientsSay/>
+        
+        </div>
+  )
+}
+
+export default page
