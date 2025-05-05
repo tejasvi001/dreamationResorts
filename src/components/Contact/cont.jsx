bdc07abb030f8705ff7a456d116571f224604c25
lakshay <lakshay8219@gmail.com>
2025-05-05 19:00:01 +0530

    code merging

--- src/components/Contact/cont.jsx
+++ src/components/Contact/cont.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Contact = () => {
   return (
-    <div className="relative w-full min-h-screen flex items-center justify-center bg-gray-900">
+    <div className="relative w-full min-h-screen flex items-center justify-center">
       {/* Background image with overlay */}
-      <div className=" inset-0">
+      <div className="inset-0">
         <Image
           src="/images/ResidentialRoom/Rectangle 8.png"
           alt="Spa Image"
@@ -17,28 +17,31 @@ const Contact = () => {
       {/* Content wrapper */}
       <div className="relative z-10 text-white w-full px-4 sm:px-8 lg:px-20 py-20">
         {/* Title */}
-        <h1 className="text-4xl md:text-6xl font-semibold text-center mb-16">Contact Us</h1>
+        <h1 className="text-5xl md:text-6xl font-semibold text-center mt-8 md:mt-16">
+          Contact Us
+        </h1>
 
         {/* Main content */}
         <div className="flex flex-col lg:flex-row items-center ">
           {/* Image section */}
-                <div className="  relative mt-12 lg:mt-58 order-last lg:order-first  lg:mr- w-full lg:w-1/2">
-                <Image
-                  src="/images/Spaimg/Rectangle 85.png"
-                  alt="Spa card Image"
-                  width={1200}
-                  height={900}
-                  className=" relative shadow-lg w-full h-auto rounded-xl "
-                />
-                </div>
+          <div className="  relative mt-16 lg:mt-58 order-last lg:order-first  w-full lg:w-1/2">
+            <Image
+              src="/images/Spaimg/Rectangle 85.png"
+              alt="Spa card Image"
+              width={1200}
+              height={900}
+              className=" relative shadow-lg w-full h-auto rounded-xl "
+            />
+          </div>
 
-                {/* Text section */}
-          <div className="bg-[#D69D52] p-8 w-full lg:w-1/2 text-left rounded-xl shadow-md mt-90 ">
+          {/* Text section */}
+          <div className="bg-[#D69D52] p-8 w-full lg:w-1/2 text-left rounded-xl shadow-md mt-20 absolute bottom-16 right-0">
             <h2 className="text-3xl md:text-4xl mb-4">Contat Us Today</h2>
             <h1 className="text-2xl md:text-3xl">Book Your Next</h1>
             <h1 className="text-2xl md:text-3xl mb-6">Vacation</h1>
             <p className="leading-relaxed text-sm md:text-base">
-              Mauris varius interdum erat, et ultrices justo imperdiet nec. <br />
+              Mauris varius interdum erat, et ultrices justo imperdiet nec.{" "}
+              <br />
               Cras ac porttitor tortor, id iaculis neque. <br />
               Fusce et malesuada nibh, et consectetur risus. <br />
               Suspendisse dictum orci eget cursus viverra. <br />
