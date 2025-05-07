0de9d2b62d2bc2f3d2d1a2ab77db617484def9f5
220720 <prempandey0722@gmail.com>
2025-05-07 12:56:53 +0530

    changes

--- src/components/Activities/GalleryItemRight.jsx
+++ src/components/Activities/GalleryItemRight.jsx
@@ -6,28 +6,28 @@ const GalleryItemRight = ({ image, title, description }) => {
   const router = useRouter();
 
   return (
-    <div className="relative w-full md:w-1/2 mt-[-5vh] md:mt-[-11vh] lg:ml-auto">
+    <div className="relative w-full sm:w-1/2 mt-[-5vh] sm:mt-[-11vh] sm:ml-auto">
       {/* Image */}
       <Image
         src={image}
         alt={title}
         width={960}
         height={640}
-        className="w-full h-[60vh] md:h-[80vh]  object-cover"
+        className="w-full h-[60vh] sm:h-[80vh]  object-cover"
       />
 
       {/* Text Box */}
       <div
-        className="lg:absolute lg:right-[85%] lg:top-1/2 lg:transform lg:-translate-y-1/2
-        bg-black text-white p-6 sm:p-8 md:p-10 rounded-lg shadow-lg 
-        w-full md:w-[400px] lg:w-[450px] xl:w-[500px] 
-        flex flex-col justify-center items-center text-center md:items-center md:text-center 
-        mt-6 md:mt-6"
+        className="sm:absolute sm:right-[85%] sm:top-1/2 sm:transform sm:-translate-y-1/2
+        bg-black text-white p-6 sm:p-10 rounded-lg shadow-lg 
+        w-full sm:w-[400px]  xl:w-[500px] 
+        flex flex-col justify-center items-center text-center sm:items-center sm:text-center 
+        mt-6 sm:mt-6"
       >
-        <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 md:mb-6">{title}</h3>
-        <p className="text-sm sm:text-base md:text-sm mb-4">{description}</p>
+        <h3 className="text-xl  sm:text-3xl font-semibold mb-4 sm:mb-6">{title}</h3>
+        <p className="text-sm  sm:text-sm mb-4">{description}</p>
         <button
-          className="w-full sm:w-[200px] py-2 border border-white rounded-full text-white 
+          className="w-full  py-2 border border-white rounded-full text-white 
           hover:bg-white hover:text-black transition-all"
           onClick={() => router.push("/Contact")}
         >
--- src/components/Activities/Topbox.jsx
+++ src/components/Activities/Topbox.jsx
@@ -1,15 +1,37 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
 
-const Topbox = () => {
+const HeroSection = ({HomeRef}) => {  
+  const backgroundImages = ["/activities/xgvgq4sol9lhmxwby2ye.webp",   "/activities/xkwaq8gonythkoznkxxd.webp"]; 
+  const [bgIndex, setBgIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBgIndex((prevIndex) => (prevIndex + 1) % backgroundImages.length);
+    }, 4000); 
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <nav className="relative max-w-full bg-gradient-to-b from-[#E9D0B4] to-white px-12 py-20 flex flex-col items-center justify-center overflow-x-hidden">
-      <h2 
-        className="md:text-[105px] text-7xl text-center mt-10 "
-      >
-        Activities Gallery
-      </h2>
-    </nav>
+    <div
+      className="relative w-full h-screen bg-cover bg-center  transition-all duration-1000 flex flex-col items-center justi"
+      style={{ backgroundImage: `url('${backgroundImages[bgIndex]}') ` }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 flex flex-col items-center justify-evenly md:justify-center text-center px-4 md:mt-[5%] gap-[6vw]">
+        <div>
+          <h1 className="text-4xl sm:text-4xl md:text-6xl text-white font-bold hover:scale-125 duration-2000 cursor-pointer"
+          style={{fontFamily:'memv1'}}>
+            {/* <span className="text-[#d69e54]">Welcome</span> to <br /> */}
+            <span className="text-white">Activites</span>
+          </h1>
+        </div>
+        <div className="pt-10">
+          
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default Topbox;
+export default HeroSection;
